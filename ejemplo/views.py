1921d2d65f659c52b6b6ce1c9c8be1b9ba3d4975
Yenicee Vazquez <perezyenice@gmail.com>
2022-10-16 17:30:03 -0300

    agrego ejemplo de clases 18

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,5 +1,33 @@
+from http.client import ImproperConnectionState
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html",
+    {
+     "nombre":"Yenice",
+      "apellido":"Vazquez Perez"
+    })
+
+
+def index_dos(request, nombre, apellido):
+    suma = 1+1
+    return render(request, "ejemplo/saludar.html",
+    {
+        "nombre":"yenice",
+        "apellido":suma,
+    })
+
+def index_tres(request):
+
+    return render(request, "ejemplo/saludar.html",
+    {"notas": [1,2,3,4,5,6,7,8]}
+    )
+
+def imc(request, peso, altura):
+    imc= 1 #calcular esto
+    return render(request, "ejemplo/imc.html", {"imc":imc}) 
     
+
+
+
+
